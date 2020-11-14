dcd80ec5a02cacb56602a36f365875749b31a88c
Daniil Dorovenko <dorovenco@mail.ru>
2020-11-14 10:34:58 +0300

    raplace dirs and fix some bugs

--- stepik_tours/stepik_tours/settings.py
+++ stepik_tours/stepik_tours/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't(v8mg)siltr_^$*f9@f&m2%0!9u0yf1utu%!224q8++gda&i&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,12 +74,12 @@ WSGI_APPLICATION = 'stepik_tours.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
